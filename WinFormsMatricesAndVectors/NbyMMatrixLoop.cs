bc7d0db8c4fadbe81400d79dd56fe2764cc3a984
David Morrow <quickbasic44@outlook.com>
2023-04-11 18:38:04 -0400

    Using for loops to work matrix buttons

--- WinFormsMatricesAndVectors/NbyMMatrixLoop.cs
+++ WinFormsMatricesAndVectors/NbyMMatrixLoop.cs
@@ -44,77 +44,212 @@ namespace WinFormsMatricesAndVectors
 
         private void button2_Click(object sender, EventArgs e)
         {
-            // Code for button2_Click event handler
+            int count = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    buttons[i, j].BackColor = Color.Red;
+                    buttons[i, j].Text = "Matrix" + count.ToString();
+                    count++;
+                }
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            // Code for button3_Click event handler
+            int count = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    buttons[i, j].BackColor = Color.Red;
+                    buttons[i, j].Text = "Matrix" + count.ToString();
+                    count++;
+                }
+            }
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            // Code for button4_Click event handler
+            int count = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    buttons[i, j].BackColor = Color.Red;
+                    buttons[i, j].Text = "Matrix" + count.ToString();
+                    count++;
+                }
+            }
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            // Code for button5_Click event handler
+            int count = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    buttons[i, j].BackColor = Color.Red;
+                    buttons[i, j].Text = "Matrix" + count.ToString();
+                    count++;
+                }
+            }
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            // Code for button6_Click event handler
+            int count = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    buttons[i, j].BackColor = Color.Red;
+                    buttons[i, j].Text = "Matrix" + count.ToString();
+                    count++;
+                }
+            }
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            // Code for button7_Click event handler
+            int count = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    buttons[i, j].BackColor = Color.Red;
+                    buttons[i, j].Text = "Matrix" + count.ToString();
+                    count++;
+                }
+            }
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-            // Code for button8_Click event handler
+            int count = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    buttons[i, j].BackColor = Color.Red;
+                    buttons[i, j].Text = "Matrix" + count.ToString();
+                    count++;
+                }
+            }
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-            // Code for button9_Click event handler
+            int count = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    buttons[i, j].BackColor = Color.Red;
+                    buttons[i, j].Text = "Matrix" + count.ToString();
+                    count++;
+                }
+            }
         }
 
         private void button10_Click(object sender, EventArgs e)
         {
-            // Code for button10_Click event handler
+            int count = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    buttons[i, j].BackColor = Color.Red;
+                    buttons[i, j].Text = "Matrix" + count.ToString();
+                    count++;
+                }
+            }
         }
 
         private void button11_Click(object sender, EventArgs e)
         {
-            // Code for button11_Click event handler
+            int count = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    buttons[i, j].BackColor = Color.Red;
+                    buttons[i, j].Text = "Matrix" + count.ToString();
+                    count++;
+                }
+            }
         }
 
         private void button12_Click(object sender, EventArgs e)
         {
-            // Code for button12_Click event handler
+            int count = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    buttons[i, j].BackColor = Color.Red;
+                    buttons[i, j].Text = "Matrix" + count.ToString();
+                    count++;
+                }
+            }
         }
 
         private void button13_Click(object sender, EventArgs e)
         {
-            // Code for button13_Click event handler
+            int count = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    buttons[i, j].BackColor = Color.Red;
+                    buttons[i, j].Text = "Matrix" + count.ToString();
+                    count++;
+                }
+            }
         }
 
         private void button14_Click(object sender, EventArgs e)
         {
-            // Code for button14_Click event handler
+            int count = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    buttons[i, j].BackColor = Color.Red;
+                    buttons[i, j].Text = "Matrix" + count.ToString();
+                    count++;
+                }
+            }
         }
 
         private void button15_Click(object sender, EventArgs e)
         {
-            // Code for button15_Click event handler
+            int count = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    buttons[i, j].BackColor = Color.Red;
+                    buttons[i, j].Text = "Matrix" + count.ToString();
+                    count++;
+                }
+            }
         }
 
         private void button16_Click(object sender, EventArgs e)
         {
-            // Code for button16_Click event handler
+            int count = 1;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    buttons[i, j].BackColor = Color.Red;
+                    buttons[i, j].Text = "Matrix" + count.ToString();
+                    count++;
+                }
+            }
         }
 
 

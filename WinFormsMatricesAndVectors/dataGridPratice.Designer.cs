5af704c0668703012debbf66397de8beaddf8a13
David Morrow <quickbasic44@outlook.com>
2023-04-20 17:19:51 -0400

    2 dataGridViews and matrices connected to them

--- WinFormsMatricesAndVectors/dataGridPratice.Designer.cs
+++ WinFormsMatricesAndVectors/dataGridPratice.Designer.cs
@@ -28,33 +28,217 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle5 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle6 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle7 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle8 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle9 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle10 = new DataGridViewCellStyle();
             dataGridView1 = new DataGridView();
+            label1 = new Label();
+            dataGridView2 = new DataGridView();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
             SuspendLayout();
             // 
             // dataGridView1
             // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.TopLeft;
+            dataGridViewCellStyle1.BackColor = SystemColors.Control;
+            dataGridViewCellStyle1.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = SystemColors.Control;
+            dataGridViewCellStyle1.Format = "N0";
+            dataGridViewCellStyle1.NullValue = "0";
+            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Control;
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.Control;
+            dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridView1.Anchor = AnchorStyles.None;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.BackgroundColor = SystemColors.Control;
+            dataGridView1.CausesValidation = false;
+            dataGridView1.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText;
+            dataGridView1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.TopLeft;
+            dataGridViewCellStyle2.BackColor = SystemColors.Control;
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = SystemColors.Control;
+            dataGridViewCellStyle2.Format = "N0";
+            dataGridViewCellStyle2.NullValue = null;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Control;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.Control;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(102, 95);
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.TopLeft;
+            dataGridViewCellStyle3.BackColor = SystemColors.Control;
+            dataGridViewCellStyle3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = Color.Black;
+            dataGridViewCellStyle3.Format = "N0";
+            dataGridViewCellStyle3.NullValue = "0";
+            dataGridViewCellStyle3.SelectionBackColor = SystemColors.Control;
+            dataGridViewCellStyle3.SelectionForeColor = SystemColors.Control;
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.False;
+            dataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            dataGridView1.GridColor = Color.Blue;
+            dataGridView1.Location = new Point(828, 109);
+            dataGridView1.Margin = new Padding(4);
             dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.TopLeft;
+            dataGridViewCellStyle4.BackColor = SystemColors.Control;
+            dataGridViewCellStyle4.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle4.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle4.Format = "N0";
+            dataGridViewCellStyle4.NullValue = "0";
+            dataGridViewCellStyle4.SelectionBackColor = SystemColors.Control;
+            dataGridViewCellStyle4.SelectionForeColor = SystemColors.Control;
+            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.True;
+            dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridView1.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders;
+            dataGridViewCellStyle5.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle5.Format = "N0";
+            dataGridViewCellStyle5.NullValue = "0";
+            dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle5;
+            dataGridView1.RowTemplate.DefaultCellStyle.BackColor = SystemColors.Control;
+            dataGridView1.RowTemplate.DefaultCellStyle.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridView1.RowTemplate.DefaultCellStyle.ForeColor = Color.Black;
+            dataGridView1.RowTemplate.DefaultCellStyle.Format = "N0";
+            dataGridView1.RowTemplate.DefaultCellStyle.NullValue = "0";
+            dataGridView1.RowTemplate.DefaultCellStyle.SelectionBackColor = Color.FromArgb(255, 128, 0);
+            dataGridView1.RowTemplate.DefaultCellStyle.SelectionForeColor = Color.Black;
             dataGridView1.RowTemplate.Height = 25;
-            dataGridView1.Size = new Size(503, 288);
+            dataGridView1.RowTemplate.Resizable = DataGridViewTriState.True;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.CellSelect;
+            dataGridView1.ShowCellErrors = false;
+            dataGridView1.Size = new Size(500, 150);
+            dataGridView1.StandardTab = true;
             dataGridView1.TabIndex = 0;
+            dataGridView1.DoubleClick += dataGridView1_DoubleClick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(903, 34);
+            label1.Name = "label1";
+            label1.Size = new Size(57, 21);
+            label1.TabIndex = 1;
+            label1.Text = "label1";
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.AllowUserToAddRows = false;
+            dataGridView2.AllowUserToDeleteRows = false;
+            dataGridView2.AllowUserToResizeColumns = false;
+            dataGridView2.AllowUserToResizeRows = false;
+            dataGridViewCellStyle6.Alignment = DataGridViewContentAlignment.TopLeft;
+            dataGridViewCellStyle6.BackColor = SystemColors.Control;
+            dataGridViewCellStyle6.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle6.ForeColor = SystemColors.Control;
+            dataGridViewCellStyle6.Format = "N0";
+            dataGridViewCellStyle6.NullValue = "0";
+            dataGridViewCellStyle6.SelectionBackColor = SystemColors.Control;
+            dataGridViewCellStyle6.SelectionForeColor = SystemColors.Control;
+            dataGridView2.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle6;
+            dataGridView2.Anchor = AnchorStyles.None;
+            dataGridView2.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            dataGridView2.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView2.BackgroundColor = SystemColors.Control;
+            dataGridView2.CausesValidation = false;
+            dataGridView2.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText;
+            dataGridView2.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle7.Alignment = DataGridViewContentAlignment.TopLeft;
+            dataGridViewCellStyle7.BackColor = SystemColors.Control;
+            dataGridViewCellStyle7.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle7.ForeColor = SystemColors.Control;
+            dataGridViewCellStyle7.Format = "N0";
+            dataGridViewCellStyle7.NullValue = null;
+            dataGridViewCellStyle7.SelectionBackColor = SystemColors.Control;
+            dataGridViewCellStyle7.SelectionForeColor = SystemColors.Control;
+            dataGridViewCellStyle7.WrapMode = DataGridViewTriState.True;
+            dataGridView2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle8.Alignment = DataGridViewContentAlignment.TopLeft;
+            dataGridViewCellStyle8.BackColor = SystemColors.Control;
+            dataGridViewCellStyle8.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle8.ForeColor = Color.Black;
+            dataGridViewCellStyle8.Format = "N0";
+            dataGridViewCellStyle8.NullValue = "0";
+            dataGridViewCellStyle8.SelectionBackColor = SystemColors.Control;
+            dataGridViewCellStyle8.SelectionForeColor = SystemColors.Control;
+            dataGridViewCellStyle8.WrapMode = DataGridViewTriState.False;
+            dataGridView2.DefaultCellStyle = dataGridViewCellStyle8;
+            dataGridView2.GridColor = Color.Blue;
+            dataGridView2.Location = new Point(828, 326);
+            dataGridView2.Margin = new Padding(4);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle9.Alignment = DataGridViewContentAlignment.TopLeft;
+            dataGridViewCellStyle9.BackColor = SystemColors.Control;
+            dataGridViewCellStyle9.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle9.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle9.Format = "N0";
+            dataGridViewCellStyle9.NullValue = "0";
+            dataGridViewCellStyle9.SelectionBackColor = SystemColors.Control;
+            dataGridViewCellStyle9.SelectionForeColor = SystemColors.Control;
+            dataGridViewCellStyle9.WrapMode = DataGridViewTriState.True;
+            dataGridView2.RowHeadersDefaultCellStyle = dataGridViewCellStyle9;
+            dataGridView2.RowHeadersWidthSizeMode = DataGridViewRowHeadersWidthSizeMode.AutoSizeToDisplayedHeaders;
+            dataGridViewCellStyle10.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridViewCellStyle10.Format = "N0";
+            dataGridViewCellStyle10.NullValue = "0";
+            dataGridView2.RowsDefaultCellStyle = dataGridViewCellStyle10;
+            dataGridView2.RowTemplate.DefaultCellStyle.BackColor = SystemColors.Control;
+            dataGridView2.RowTemplate.DefaultCellStyle.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            dataGridView2.RowTemplate.DefaultCellStyle.ForeColor = Color.Black;
+            dataGridView2.RowTemplate.DefaultCellStyle.Format = "N0";
+            dataGridView2.RowTemplate.DefaultCellStyle.NullValue = "0";
+            dataGridView2.RowTemplate.DefaultCellStyle.SelectionBackColor = Color.FromArgb(255, 128, 0);
+            dataGridView2.RowTemplate.DefaultCellStyle.SelectionForeColor = Color.Black;
+            dataGridView2.RowTemplate.Height = 25;
+            dataGridView2.RowTemplate.Resizable = DataGridViewTriState.True;
+            dataGridView2.SelectionMode = DataGridViewSelectionMode.CellSelect;
+            dataGridView2.ShowCellErrors = false;
+            dataGridView2.Size = new Size(500, 150);
+            dataGridView2.StandardTab = true;
+            dataGridView2.TabIndex = 2;
             // 
             // dataGridPratice
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            AutoScroll = true;
+            AutoSize = true;
+            AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            AutoValidate = AutoValidate.EnableAllowFocusChange;
+            ClientSize = new Size(1547, 630);
+            Controls.Add(dataGridView2);
+            Controls.Add(label1);
             Controls.Add(dataGridView1);
+            Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            FormBorderStyle = FormBorderStyle.SizableToolWindow;
+            Margin = new Padding(4);
             Name = "dataGridPratice";
             Text = "dataGridPratice";
+            WindowState = FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private DataGridView dataGridView1;
+        private Label label1;
+        private DataGridView dataGridView2;
     }
 }
--- WinFormsMatricesAndVectors/dataGridPratice.cs
+++ WinFormsMatricesAndVectors/dataGridPratice.cs
@@ -14,17 +14,23 @@ namespace WinFormsMatricesAndVectors
 {
     public partial class dataGridPratice : Form
     {
+        MatrixBuilder<double> basicMatrix = Matrix<double>.Build;
+        Matrix<double> rowMatrix;
         public dataGridPratice()
         {
             InitializeComponent();
-            Matrix<double> rowMatrix = Matrix<double>.Build.DenseOfColumnArrays(new[] { 1.0, 2.0, 3.0, 4.0 }, new[] { 5.0, 6.0, 7.0, 8.0 }, new[] { 11.0, 12.0, 13.0, 14.0 }, new[] { 15.0, 16.0, 17.0, 18.0 });
+            rowMatrix = basicMatrix.DenseOfColumnArrays(
+         new[] { 1.0, 2.0, 3.0, 4.0 },
+         new[] { 5.0, 6.0, 7.0, 8.0 },
+         new[] { 11.0, 12.0, 13.0, 14.0 },
+         new[] { 15.0, 16.0, 17.0, 18.0 });
 
             Matrix<double> columnMatrix = Matrix<double>.Build.DenseOfArray(new double[,]
             {
-    { 1, 2, 3, 4 },
-    { 4, 5, 6, 7 },
-    { 7, 8, 9, 10 },
-    { 11, 12, 13, 14 },
+                    { 1, 2, 3, 4 },
+                    { 4, 5, 6, 7 },
+                    { 7, 8, 9, 10 },
+                    { 11, 12, 13, 14 },
             });
             dataGridView1.AllowUserToAddRows = false;
             dataGridView1.Columns.Add("Column1", "Column 1");
@@ -32,40 +38,47 @@ namespace WinFormsMatricesAndVectors
             dataGridView1.Columns.Add("Column3", "Column 3");
             dataGridView1.Columns.Add("Column4", "Column 4");
             dataGridView1.Rows.Add(4);
-            CopyMatrixToDataGrid(rowMatrix);
-            
+
+            CopyMatrixToDataGrid(rowMatrix, dataGridView1);
+
 
         }
 
-        void CopyMatrixToDataGrid(Matrix<double> m1)
+        void CopyMatrixToDataGrid(Matrix<double> m1, DataGridView dataGridView)
         {
-            dataGridView1.RowCount = m1.RowCount;
-            dataGridView1.ColumnCount = m1.ColumnCount;
+            dataGridView.RowCount = m1.RowCount;
+            dataGridView.ColumnCount = m1.ColumnCount;
             // Set cell values in the DataTable
             for (int i = 0; i < m1.RowCount; i++)
             {
                 for (int j = 0; j < m1.ColumnCount; j++)
                 {
-                    DataGridViewCell cell = dataGridView1[i, j];
+                    DataGridViewCell cell = dataGridView[i, j];
                     dataGridView1 = cell.DataGridView;
                     cell.Value = m1[i, j];
                 }
             }
         }
 
-        void CopyDataGridToMatrix(Matrix<double> m1)
+        void CopyDataGridToMatrix(Matrix<double> m1,DataGridView dataGridView)
         {
-            dataGridView1.RowCount = m1.RowCount;
-            dataGridView1.ColumnCount = m1.ColumnCount;
+            dataGridView.RowCount = m1.RowCount;
+            dataGridView.ColumnCount = m1.ColumnCount;
             // Set cell values in the DataTable
-            for (int i = 0; i < dataGridView1.RowCount; i++)
+            for (int i = 0; i < dataGridView.RowCount; i++)
             {
-                for (int j = 0; j < dataGridView1.ColumnCount; j++)
+                for (int j = 0; j < dataGridView.ColumnCount; j++)
                 {
-                    DataGridViewCell cell = dataGridView1[i, j];
+                    DataGridViewCell cell = dataGridView[i, j];
                     m1[i, j] = double.Parse(cell.Value.ToString());
                 }
             }
+        }
+
+        private void dataGridView1_DoubleClick(object sender, EventArgs e)
+        {
+            label1.Text = "DataGrid copied to Matrix";
+            CopyMatrixToDataGrid(rowMatrix, dataGridView2);
         }
     }
 }

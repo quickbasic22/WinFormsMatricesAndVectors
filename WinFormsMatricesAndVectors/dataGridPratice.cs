a3e0f6293be7c6b10379766e5a51e70be255be0f
David Morrow <quickbasic44@outlook.com>
2023-04-26 17:45:04 -0400

    Added MenuStrip to Create Matrices with

--- WinFormsMatricesAndVectors/dataGridPratice.cs
+++ WinFormsMatricesAndVectors/dataGridPratice.cs
@@ -29,13 +29,13 @@ namespace WinFormsMatricesAndVectors
          new[] { 11.0, 12.0, 13.0, 14.0 },
          new[] { 15.0, 16.0, 17.0, 18.0 });
 
-              MatrixB = Matrix<double>.Build.DenseOfArray(new double[,]
-            {
+            MatrixB = Matrix<double>.Build.DenseOfArray(new double[,]
+          {
                     { 1, 2, 3, 4 },
                     { 4, 5, 6, 7 },
                     { 7, 8, 9, 10 },
                     { 11, 12, 13, 14 },
-            });
+          });
             dataGridView1.Rows.Clear();
             dataGridView1.AllowUserToAddRows = false;
             dataGridView1.Columns.Add("Column1", "Column 1");
@@ -101,10 +101,10 @@ namespace WinFormsMatricesAndVectors
             }
         }
 
-        void CopyDataGridToMatrix(Matrix<double> m1,DataGridView dataGridView)
+        void CopyDataGridToMatrix(Matrix<double> m1, DataGridView dataGridView)
         {
-            
-           // Set cell values in the DataTable
+
+            // Set cell values in the DataTable
             for (int i = 0; i < dataGridView.RowCount; i++)
             {
                 for (int j = 0; j < dataGridView.ColumnCount; j++)
@@ -193,6 +193,84 @@ namespace WinFormsMatricesAndVectors
                     break;
             }
 
+        }
+
+        private void Matrix2x2Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView2.Rows.Clear();
+            dataGridView3.Rows.Clear();
+            dataGridView1.RowCount = 2;
+            dataGridView1.ColumnCount = 2;
+            dataGridView2.RowCount = 2;
+            dataGridView2.ColumnCount = 2;
+            dataGridView3.RowCount = 2;
+            dataGridView3.ColumnCount = 2;
+            MatrixA.Clear();
+            
+           MatrixA = Matrix<double>.Build.DenseOfColumnArrays(
+               new[] { 2.0, 3.0 },
+               new[] { 5.0, 6.0 });
+
+            MatrixB = Matrix<double>.Build.DenseOfColumnArrays(
+              new[] { 2.0, 3.0 },
+              new[] { 5.0, 6.0 });
+
+            CopyMatrixToDataGrid(MatrixA, dataGridView1);
+            CopyMatrixToDataGrid(MatrixB, dataGridView2);
+
+        }
+
+        private void Matrix3x3Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView2.Rows.Clear();
+            dataGridView3.Rows.Clear();
+            dataGridView1.RowCount = 3;
+            dataGridView1.ColumnCount = 3;
+            dataGridView2.RowCount = 3;
+            dataGridView2.ColumnCount = 3;
+            dataGridView3.RowCount = 3;
+            dataGridView3.ColumnCount = 3;
+            MatrixA.Clear();
+
+            MatrixA = Matrix<double>.Build.DenseOfColumnArrays(
+                new[] { 2.0, 3.0, 4.0 },
+                new[] { 5.0, 6.0, 7.0 },
+                new[] { 11.0, 12,0, 13.0 });
+
+            MatrixB = Matrix<double>.Build.DenseOfColumnArrays(
+                new[] { 2.0, 3.0, 4.0 },
+                new[] { 5.0, 6.0, 7.0 },
+                new[] { 11.0, 12, 0, 13.0 });
+            CopyMatrixToDataGrid(MatrixA, dataGridView1);
+            CopyMatrixToDataGrid(MatrixB, dataGridView2);
+        }
+
+        private void Matrix4x4(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView2.Rows.Clear();
+            dataGridView3.Rows.Clear();
+            dataGridView1.RowCount = 4;
+            dataGridView1.ColumnCount = 4;
+            dataGridView2.RowCount = 4;
+            dataGridView2.ColumnCount = 4;
+            dataGridView3.RowCount = 4;
+            dataGridView3.ColumnCount = 4;
+            MatrixA.Clear();
+
+            MatrixA = Matrix<double>.Build.DenseOfColumnArrays(
+                new[] { 2.0, 3.0, 4.0, 5.0 },
+                new[] { 5.0, 6.0, 7.0, 8.0 },
+                new[] { 11.0, 12, 0, 13.0, 14.0 });
+
+            MatrixB = Matrix<double>.Build.DenseOfColumnArrays(
+                new[] { 2.0, 3.0, 4.0 },
+                new[] { 5.0, 6.0, 7.0 },
+                new[] { 11.0, 12, 0, 13.0 });
+            CopyMatrixToDataGrid(MatrixA, dataGridView1);
+            CopyMatrixToDataGrid(MatrixB, dataGridView2);
         }
     }
 }
